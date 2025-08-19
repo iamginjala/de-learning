3aa2dd344892f5447eb39eb3be9b9562d77b174e
Harsha <harshaginjaladec23@gmail.com>
2025-08-19 15:06:43 -0400

    slicing operations

--- python_practice/numpy-basics.py
+++ python_practice/numpy-basics.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 #creating array 
-array = np.array([1,2,3,4])
-print(array.ndim)
-print(type(array))
+array1 = np.array([1,2,3,4])
+print(array1.ndim)
+print(type(array1))
 
 # basic array operations
-array *= 2
-print(array) 
+array1 *= 2
+print(array1) 
 
 # multi dimensional arrays
 
@@ -15,7 +15,23 @@ array_3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],
                     [[1,2,3],[4,5,6],[7,8,9]],
                     [[1,2,3],[4,5,6],[7,8,9]]])
 # accessing elements
-
-
 number  = array_3d[0,1,0] +array_3d[1,0,2] + array_3d[2,1,1]
 print(number)
+
+# slicing numpy array
+
+array = np.array([[1,2,3,4],
+                  [5,6,7,8],
+                  [9,10,11,12],
+                  [13,14,15,16]])
+
+# array[start:end:step]
+#  accessing subset of array
+
+print(array[:2,2:])
+
+# for accessing individual rows and columns
+
+print(array[0])
+
+print(array[:,::-2])
