0ff1cf2f1ae093007e28df2c0d8ecea1849c43ea
Harsha <harshaginjaladec23@gmail.com>
2025-08-19 14:26:17 -0400

    multi dimensional arrays

--- python_practice/numpy-basics.py
+++ python_practice/numpy-basics.py
@@ -1,10 +1,21 @@
 import numpy as np
 
-# creating array 
+#creating array 
 array = np.array([1,2,3,4])
-print(array)
+print(array.ndim)
 print(type(array))
 
 # basic array operations
 array *= 2
 print(array) 
+
+# multi dimensional arrays
+
+array_3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],
+                    [[1,2,3],[4,5,6],[7,8,9]],
+                    [[1,2,3],[4,5,6],[7,8,9]]])
+# accessing elements
+
+
+number  = array_3d[0,1,0] +array_3d[1,0,2] + array_3d[2,1,1]
+print(number)
