77c4fbd90a320dc538f8a2e1dab3b79a810d2ce3
Harsha <harshaginjaladec23@gmail.com>
2025-08-20 15:02:23 -0500

    S3 operations using boto3 - upload, download, list objects

--- aws_practice/aws-operations.py
+++ aws_practice/aws-operations.py
@@ -10,4 +10,17 @@ client = boto3.client('s3')  # Uses your CLI-configured us-east-2 region
 
 # print(response)
 
-client.upload_file('aws_practice/hello.txt', 'demo-boto3-bucket-harsha-de', 'hello.txt')
+## uploading files to s3
+
+# client.upload_file('aws_practice/hello.txt', 'demo-boto3-bucket-harsha-de', 'hello.txt')
+
+## listing all s3 buckets
+
+response = client.list_buckets()
+
+if response['Buckets']:
+    print("Buckets found:")
+    for bucket in response['Buckets']:
+        print(bucket['Name'])
+else:
+    print("No buckets found.")
