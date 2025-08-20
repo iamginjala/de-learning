964b92cc3a7952a43cc2743e720dd6f875f33a25
Harsha <harshaginjaladec23@gmail.com>
2025-08-20 15:48:10 -0500

    S3 operations using boto3 - upload, download, list objects

--- aws_practice/aws-operations.py
+++ aws_practice/aws-operations.py
@@ -1,4 +1,11 @@
 import boto3
+import os
+
+folder = 'C:/Users/hr242/Desktop/de-learning/aws_practice'
+filename = 'downloaded_s3_file.txt'
+
+full_path = os.path.join(folder, filename)
+
 
 client = boto3.client('s3')  # Uses your CLI-configured us-east-2 region
 # response = client.create_bucket(
@@ -16,11 +23,21 @@ client = boto3.client('s3')  # Uses your CLI-configured us-east-2 region
 
 ## listing all s3 buckets
 
-response = client.list_buckets()
+# response = client.list_buckets()
 
-if response['Buckets']:
-    print("Buckets found:")
-    for bucket in response['Buckets']:
-        print(bucket['Name'])
-else:
-    print("No buckets found.")
+# if response['Buckets']:
+#     print("Buckets found:")
+#     for bucket in response['Buckets']:
+#         print(bucket['Name'])
+# else:
+#     print("No buckets found.")
+
+## list all objects from s3
+response = client.list_objects(Bucket = 'demo-boto3-bucket-harsha-de')
+
+if response['Contents']:
+    for obj in response['Contents']:
+        print(obj['Key'])
+
+## download_file 
+client.download_file('demo-boto3-bucket-harsha-de','hello.txt',full_path)
