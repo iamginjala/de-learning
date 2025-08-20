a2c55498fb10afae1d96f258f610dfc2ccddc097
Harsha <harshaginjaladec23@gmail.com>
2025-08-20 11:36:50 -0500

    handling missing values

--- python_practice/numpy-basics.py
+++ python_practice/numpy-basics.py
@@ -1,83 +1,83 @@
 import numpy as np
 
-# #creating array 
-# array1 = np.array([1,2,3,4])
-# print(array1.ndim)
-# print(type(array1))
+#creating array 
+array1 = np.array([1,2,3,4])
+print(array1.ndim)
+print(type(array1))
 
-# # basic array operations
-# array1 *= 2
-# print(array1) 
+# basic array operations
+array1 *= 2
+print(array1) 
 
-# # multi dimensional arrays
+# multi dimensional arrays
 
-# array_3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],
-#                     [[1,2,3],[4,5,6],[7,8,9]],
-#                     [[1,2,3],[4,5,6],[7,8,9]]])
-# # accessing elements
-# number  = array_3d[0,1,0] +array_3d[1,0,2] + array_3d[2,1,1]
-# print(number)
+array_3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],
+                    [[1,2,3],[4,5,6],[7,8,9]],
+                    [[1,2,3],[4,5,6],[7,8,9]]])
+# accessing elements
+number  = array_3d[0,1,0] +array_3d[1,0,2] + array_3d[2,1,1]
+print(number)
 
-# # slicing numpy array
+# slicing numpy array
 
-# array = np.array([[1,2,3,4],
-#                   [5,6,7,8],
-#                   [9,10,11,12],
-#                   [13,14,15,16]])
+array = np.array([[1,2,3,4],
+                  [5,6,7,8],
+                  [9,10,11,12],
+                  [13,14,15,16]])
 
-# # array[start:end:step]
-# #  accessing subset of array
+# array[start:end:step]
+#  accessing subset of array
 
-# print(array[:2,2:])
+print(array[:2,2:])
 
-# # for accessing individual rows and columns
+# for accessing individual rows and columns
 
-# print(array[0])
+print(array[0])
 
-# print(array[:,::-2])
+print(array[:,::-2])
 
-# # scalar arithmetic
+# scalar arithmetic
 
-# array = np.array([1,2,3])
+array = np.array([1,2,3])
 
-# print(array+1)
-# print(array-2)
-# print(array*3)
-# print(array/4)
-# print(array ** 5)
+print(array+1)
+print(array-2)
+print(array*3)
+print(array/4)
+print(array ** 5)
 
-# #vectorized math funcs
-# radii = np.array([1,2,3])
+#vectorized math funcs
+radii = np.array([1,2,3])
 
-# ## area of circle 
+## area of circle 
 
-# area = np.pi * radii**2
-# print(area)
+area = np.pi * radii**2
+print(area)
 
-# # element wise arithmetic
+# element wise arithmetic
 
-# ar1 = np.array([2,4,6])
-# ar2 = np.array([1,3,5])
-# print(ar1 ** ar2)
+ar1 = np.array([2,4,6])
+ar2 = np.array([1,3,5])
+print(ar1 ** ar2)
 
-# ## comparision operators
+## comparision operators
 
-# scores = np.array([91,55,100,73,82,46])
+scores = np.array([91,55,100,73,82,46])
 
-# scores[scores < 60] = 0
-# print(scores)
+scores[scores < 60] = 0
+print(scores)
 
-# # broadcast
+# broadcast
 
-# array2 = np.array([[1,2,3,4]])
-# array3 = np.array([[1],[2],[3],[4]])
+array2 = np.array([[1,2,3,4]])
+array3 = np.array([[1],[2],[3],[4]])
 
-# print(array2.shape)
-# print(array3.shape)
+print(array2.shape)
+print(array3.shape)
 
-# ar = array3 * array2
+ar = array3 * array2
 
-# print(ar)
+print(ar)
 
 ## handling missing values 
 
