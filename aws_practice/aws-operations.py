9d1668c46b7bf9922be4da9640cc5fa2d1eaab5c
Harsha <harshaginjaladec23@gmail.com>
2025-08-20 14:47:51 -0500

    S3 operations using boto3 - upload, download, list objects

--- aws_practice/aws-operations.py
+++ aws_practice/aws-operations.py
@@ -1,11 +1,13 @@
 import boto3
 
 client = boto3.client('s3')  # Uses your CLI-configured us-east-2 region
-response = client.create_bucket(
-    Bucket='demo-boto3-bucket-harsha-de',
-    CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2'
-    }
-)
+# response = client.create_bucket(
+#     Bucket='demo-boto3-bucket-harsha-de',
+#     CreateBucketConfiguration={
+#         'LocationConstraint': 'us-east-2'
+#     }
+# )
 
-print(response.json)
+# print(response)
+
+client.upload_file('aws_practice/hello.txt', 'demo-boto3-bucket-harsha-de', 'hello.txt')
