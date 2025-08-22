fa585d6736cc8927b5cc9371858b65a9aa64f98f
Harsha <harshaginjaladec23@gmail.com>
2025-08-22 15:55:05 -0500

    Pandas GroupBy and pivot operations on real dataset

--- python_practice/pandas-groupby.py
+++ python_practice/pandas-groupby.py
@@ -7,33 +7,51 @@ full_path = os.path.join(folder,file)
 df = pd.read_csv(full_path)
 ### Basic Exploration
 
-# What are the number of rows and columns in the dataset?
+# # What are the number of rows and columns in the dataset?
 
-print(df.shape)
+# print(df.shape)
 
 # what are the column names 
 
 print(df.columns)
 
-# How many passengers survived and how many did not?
+# # How many passengers survived and how many did not?
 
-total = df['Survived'].count()
-print(f'Survived passengers: {df["Survived"].sum()}')
-print(f'not survived passengers: {total-df["Survived"].sum()}')
+# total = df['Survived'].count()
+# print(f'Survived passengers: {df["Survived"].sum()}')
+# print(f'not survived passengers: {total-df["Survived"].sum()}')
 
-# What is the average age of passengers?
+# # What is the average age of passengers?
 
-avg_age = df['Age'].mean()
-print(avg_age)
-# What is the distribution of passengers by class (Pclass)?
+# avg_age = df['Age'].mean()
+# print(avg_age)
+# # What is the distribution of passengers by class (Pclass)?
 
-print(df['Pclass'].value_counts())
-# What is the survival rate for male vs female passengers?
+# print(df['Pclass'].value_counts())
+# # What is the survival rate for male vs female passengers?
 
-survial_rate = pd.pivot_table(df,values='Survived',index='Sex',aggfunc='mean')
-print(survial_rate)
+# survial_rate = pd.pivot_table(df,values='Survived',index='Sex',aggfunc='mean')
+# print(survial_rate)
 
-# or using group by
+# # or using group by
 
-survival = df.groupby('Sex')['Survived'].mean()
-print(survival)
+# survival = df.groupby('Sex')['Survived'].mean()
+# print(survival)
+
+## GroupBy & Aggregation
+
+# Calculate the average fare paid by each passenger class.
+print(df.groupby('Pclass')['Fare'].mean())
+
+# Find the survival rate by passenger class (Pclass).
+print(df.groupby('Pclass')['Survived'].mean())
+
+# Calculate the average age of passengers grouped by survival status (Survived).
+print(df.groupby('Survived')['Age'].mean())
+print(df.groupby(['Survived','Sex'])['Age'].mean())
+# Find the number of siblings/spouses (SibSp) and parents/children (Parch) grouped by survival status.
+# print(df.groupby('Survived')['SibSp'].sum())
+# print(df.groupby('Survived')['Parch'].sum())
+print(df.groupby('Survived')[['SibSp', 'Parch']].agg(['count', 'sum', 'mean']).round(2))
+# For each embarkation port (Embarked), calculate the survival rate.
+print(df.groupby('Embarked')['Survived'].mean())
