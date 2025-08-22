a79039e9845050730420302429986e80abffd24b
Harsha <harshaginjaladec23@gmail.com>
2025-08-22 10:41:18 -0500

    joins and sub queries

--- sql_practice/joins and subqueries.sql
+++ sql_practice/joins and subqueries.sql
@@ -71,3 +71,19 @@ ORDER BY
   G.grade DESC,
   CASE WHEN G.grade >= 8 THEN S.name END ASC,
   CASE WHEN G.grade < 8 THEN S.marks END ASC;
+
+--Ollivander's Inventory
+
+SELECT w.id, wp.age, w.coins_needed, w.power 
+FROM wands w 
+JOIN wands_property wp ON w.code = wp.code 
+WHERE wp.is_evil = 0 
+AND w.coins_needed = (
+    SELECT MIN(w2.coins_needed) 
+    FROM wands w2 
+    JOIN wands_property wp2 ON w2.code = wp2.code 
+    WHERE wp2.is_evil = 0 
+    AND w2.power = w.power 
+    AND wp2.age = wp.age
+)
+ORDER BY w.power DESC, wp.age DESC;
