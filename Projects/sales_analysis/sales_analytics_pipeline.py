f2f98db7a0e0f68cbe1634070e2d8e0656ba9e92
Harsha <harshaginjaladec23@gmail.com>
2025-08-24 14:08:38 -0500

    Setting up Databse

--- Projects/sales_analysis/sales_analytics_pipeline.py
+++ Projects/sales_analysis/sales_analytics_pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import os
+import sqlite3
 
 cwd = os.getcwd()
 full_path = os.path.join(cwd,'Projects','sales_analysis','sample_sales_data.csv')
@@ -13,35 +14,35 @@ df = pd.read_csv(full_path)
 # Look at the first few rows to confirm structure.
 # print(df.head())
 
-# Check the number of rows and columns.
-print(df.shape)
-# check name of the columns
-print(df.columns)
+# # Check the number of rows and columns.
+# print(df.shape)
+# # check name of the columns
+# print(df.columns)
 
-# Review data types for each column.
-print(df.dtypes)
+# # Review data types for each column.
+# print(df.dtypes)
 
-# Identify missing values across columns.
-print(df.isna().sum())
-# Look at unique values for categorical fields (region, sales rep, category).
-regions = df['region'].unique()
-represantive = df['sales_rep'].value_counts()
-cate = df['category'].unique()
-print(regions)
-print(represantive)
-print(cate)
+# # Identify missing values across columns.
+# print(df.isna().sum())
+# # Look at unique values for categorical fields (region, sales rep, category).
+# regions = df['region'].unique()
+# represantive = df['sales_rep'].value_counts()
+# cate = df['category'].unique()
+# print(regions)
+# print(represantive)
+# print(cate)
 
-# Verify numeric fields like quantity, unit price, and total sales for any negative or suspicious values.
-print((df['quantity'] < 0).sum())
-print((df['unit_price'] <= 0).sum())
-print((df['total_sales'] < 0).sum())
-df['total'] = df['quantity'] * df['unit_price']
-print(df['total'].equals(df['total_sales']))
-print(df[df['total'] != df['total_sales']])
-# Make sure order_id looks unique.
-print(df['order_id'].nunique())
+# # Verify numeric fields like quantity, unit price, and total sales for any negative or suspicious values.
+# print((df['quantity'] < 0).sum())
+# print((df['unit_price'] <= 0).sum())
+# print((df['total_sales'] < 0).sum())
+# df['total'] = df['quantity'] * df['unit_price']
+# print(df['total'].equals(df['total_sales']))
+# print(df[df['total'] != df['total_sales']])
+# # Make sure order_id looks unique.
+# print(df['order_id'].nunique())
 
-## data cleaning
+## 2.Data Cleaning With Pandas
 ## droping records with duplicate order_id
 
 df_cleaned = df.drop_duplicates(subset='order_id',keep='first').copy()
@@ -84,3 +85,5 @@ print(f"Missing values: {df_cleaned.isnull().sum().sum()}")
 print(f"Duplicate order_ids: {df_cleaned['order_id'].duplicated().sum()}")
 print(f"Date type: {df_cleaned['order_date'].dtype}")
 print(f"Negative quantities: {(df_cleaned['quantity'] < 0).sum()}")
+
+## 3. Database connection 
