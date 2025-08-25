f5f44ea8a06163bc9db5fcd48c19b810734f4da0
Harsha <harshaginjaladec23@gmail.com>
2025-08-25 09:48:06 -0500

    intermediate sql analysis

--- Projects/sales_analysis/sales_analytics_pipeline.py
+++ Projects/sales_analysis/sales_analytics_pipeline.py
@@ -1,15 +1,17 @@
 import pandas as pd 
 import os
 import sqlite3
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# cwd = os.getcwd()
-# full_path = os.path.join(cwd,'Projects','sales_analysis','sample_sales_data.csv')
+cwd = os.getcwd()
+full_path = os.path.join(cwd,'Projects','sales_analysis','sample_sales_data.csv')
 
 
 # ## 1. Initial Exploration (pandas)
 
 # # Load your dataset into pandas.
-# df = pd.read_csv(full_path)
+df = pd.read_csv(full_path)
 
 # # Look at the first few rows to confirm structure.
 # # print(df.head())
@@ -42,41 +44,41 @@ import sqlite3
 # # # Make sure order_id looks unique.
 # # print(df['order_id'].nunique())
 
-# ## 2.Data Cleaning With Pandas
-# ## droping records with duplicate order_id
+## 2.Data Cleaning With Pandas
+## droping records with duplicate order_id
 
-# df_cleaned = df.drop_duplicates(subset='order_id',keep='first').copy()
+df_cleaned = df.drop_duplicates(subset='order_id',keep='first').copy()
 
-# ## removing extra spaces in the sales_rep column
-# df_cleaned['sales_rep'] = df_cleaned['sales_rep'].str.strip()
+## removing extra spaces in the sales_rep column
+df_cleaned['sales_rep'] = df_cleaned['sales_rep'].str.strip()
 
-# ## converting category column into title case 
-# df_cleaned['category'] = df_cleaned['category'].str.title()
+## converting category column into title case 
+df_cleaned['category'] = df_cleaned['category'].str.title()
 
-# ## handing missing vales filling with default values 
-# df_cleaned['region'] = df_cleaned['region'].fillna('Unknown')
-# df_cleaned['sales_rep'] = df_cleaned['sales_rep'].fillna('Unassigned')
-# df_cleaned['customer_id'] = df_cleaned['customer_id'].fillna('Guest')
+## handing missing vales filling with default values 
+df_cleaned['region'] = df_cleaned['region'].fillna('Unknown')
+df_cleaned['sales_rep'] = df_cleaned['sales_rep'].fillna('Unassigned')
+df_cleaned['customer_id'] = df_cleaned['customer_id'].fillna('Guest')
 
 
-# ## Numeric Validation Columns: quantity, unit_price, total_sales
+## Numeric Validation Columns: quantity, unit_price, total_sales
 
-# # creating new column order_type with deafult values sale,refund based on quantity
+# creating new column order_type with deafult values sale,refund based on quantity
 
-# df_cleaned['order_type'] = df_cleaned['quantity'].apply(lambda x:'Refund' if x < 0 else 'Sale')
+df_cleaned['order_type'] = df_cleaned['quantity'].apply(lambda x:'Refund' if x < 0 else 'Sale')
 
-# df_cleaned['calculated_sales'] = df_cleaned['quantity'] * df_cleaned['unit_price']
-# df_cleaned['total_sales'] = df_cleaned['calculated_sales']
+df_cleaned['calculated_sales'] = df_cleaned['quantity'] * df_cleaned['unit_price']
+df_cleaned['total_sales'] = df_cleaned['calculated_sales']
 
-# df_cleaned = df_cleaned.drop('calculated_sales',axis=1)
+df_cleaned = df_cleaned.drop('calculated_sales',axis=1)
 
-# df_cleaned['corrected_date'] = pd.to_datetime(df_cleaned['order_date'],errors='coerce')
+df_cleaned['corrected_date'] = pd.to_datetime(df_cleaned['order_date'],errors='coerce')
 
-# df_cleaned=df_cleaned.dropna(subset=['corrected_date'])
+df_cleaned=df_cleaned.dropna(subset=['corrected_date'])
 
-# df_cleaned['order_date'] = df_cleaned['corrected_date']
-# # Remove the temporary corrected_date column since you no longer need it
-# df_cleaned = df_cleaned.drop('corrected_date', axis=1)
+df_cleaned['order_date'] = df_cleaned['corrected_date']
+# Remove the temporary corrected_date column since you no longer need it
+df_cleaned = df_cleaned.drop('corrected_date', axis=1)
 
 # # Verify your cleaning worked
 # # print("Final dataset info:")
@@ -149,12 +151,24 @@ cursor = conn.cursor()
 # avg_order_query_result = pd.read_sql(avg_order_query,conn)
 # print(avg_order_query_result)
 # Which customers buy the most frequently?
-cust_query = "SELECT CUSTOMER_ID ,count(*) AS PURCHASE_COUNT FROM sales_data " \
-"WHERE customer_id != 'Guest' GROUP BY customer_id ORDER BY PURCHASE_COUNT DESC"
-cust_query_result = pd.read_sql(cust_query,conn)
-print(cust_query_result)
+# cust_query = "SELECT CUSTOMER_ID ,count(*) AS PURCHASE_COUNT FROM sales_data " \
+# "WHERE customer_id != 'Guest' GROUP BY customer_id ORDER BY PURCHASE_COUNT DESC"
+# cust_query_result = pd.read_sql(cust_query,conn)
+# print(cust_query_result)
 # Are there seasonal patterns in our sales?
+# patterns_query = "SELECT strftime('%Y-%m', order_date) AS year_month,sum(total_sales) as total_sales from sales_data " \
+#                 "Group by year_month order by year_month"
+# patterns_query_result = pd.read_sql(patterns_query,conn)
+# plt.figure(figsize=(10, 6))
+# sns.lineplot(x='year_month', y='total_sales', data=patterns_query_result)
+# plt.title('Line Chart of SQLite Data')
+# plt.xlabel('Month')
+# plt.ylabel('Sale')
+# plt.grid(True)
+# plt.show()
+
 # What's the breakdown of sales vs refunds?
+print(df_cleaned.groupby('order_type')['total_sales'].sum())
 
 # Advanced Questions:
 
